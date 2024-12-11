387c5dc6eafdff17c6a8d84c093649a6747203c4
Deepak <deepak481481481@gmail.com>
2024-12-11 10:15:46 +0530

    vercel json deleted because server is hosted to the render

--- modal/auth.js
+++ modal/auth.js
@@ -1,4 +1,4 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 exports.auth_db = mongoose.createConnection(process.env.AUTH_DB);
-console.log("users db is also connected");
+console.log("users db is  connected");
